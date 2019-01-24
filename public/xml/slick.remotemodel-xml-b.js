d6c47d48389601dc5e371b045d431ea46a6ccfc3
yasuo kominami <ykominami@gmail.com>
2019-01-24 12:44:27 +0900

    rename self to this. change from function to arrow function change from var to const or let

--- public/xml/slick.remotemodel-xml-b.js
+++ public/xml/slick.remotemodel-xml-b.js
@@ -13,42 +13,40 @@
 		}
 
 		deleteData(id) {
-			var self = this;
-
 			console.log("deleteData");
-			if (self.req) {
-				self.req.abort();
-				self.data = undefined;
+			if (this.req) {
+				this.req.abort();
+				this.data = undefined;
 			}
 
-			var send_data = {
+			const send_data = {
 				id: id
 			}
-			if (self.h_request != null) {
-				clearTimeout(self.h_request);
+			if (this.h_request != null) {
+				clearTimeout(this.h_request);
 			}
 
-			self.h_request = setTimeout(function () {
-				//	    self.data = {length: 0};
-				if (("data" in self) === false) {
-					self.data = [];
-				} else if (self.data === undefined) {
-					self.data = [];
+			this.h_request = setTimeout( () => {
+				//	    this.data = {length: 0};
+				if (("data" in this) === false) {
+					this.data = [];
+				} else if (this.data === undefined) {
+					this.data = [];
 				}
-				self.data[0] = null; // null indicates a 'requested but not available yet'
+				this.data[0] = null; // null indicates a 'requested but not available yet'
 
-				self.req = $.ajax({
+				this.req = $.ajax({
 					data: send_data,
 					//		type: "POST",
 					type: "GET",
-					url: self.delete_item_url,
+					url: this.delete_item_url,
 					cache: true,
-					success: function (data) {
+					success:  (data) => {
 						// this;
-						self.onDeleteBookmarkSuccess(data)
+						this.onDeleteBookmarkSuccess(data)
 					},
-					error: function (XMLHttpRequest, textStatus, errorThrown) {
-						self.onDeleteBookmarkError()
+					error:  (XMLHttpRequest, textStatus, errorThrown) => {
+						this.onDeleteBookmarkError()
 					}
 				});
 			}, 50);
@@ -59,8 +57,8 @@
 			console.log("json.length=" + json.length);
 			if (json.length > 0) {
 				//	if (json.count > 0) {
-				var results = json
-				var item = results[0];
+				const results = json
+				const item = results[0];
 				this.data[0] = {
 					index: 0
 				};
@@ -71,10 +69,10 @@
 		}
 
 		onDeleteBookmarkError() {
-			alert("Chbk:error onDeleteBookmarkError");
+			alert("onDeleteBookmarkError:error onDeleteBookmarkError");
 		}
 
-		addData(id, name, desc, url, add_date) {
+		addData(id, name, url, title, authors, add_date) {
 			console.log("addData");
 
 			if (this.category_id == null && this.path == null) {
@@ -89,8 +87,9 @@
 			var send_data = {
 				id: id,
 				name: name,
-				desc: desc,
 				url: encodeURIComponent(url),
+				title: title,
+				authors: authors,
 				add_date: add_date
 			}
 
@@ -107,28 +106,27 @@
 			if (this.h_request != null) {
 				clearTimeout(this.h_request);
 			}
-			var self = this;
-			this.h_request = setTimeout(function () {
+			this.h_request = setTimeout( () => {
 				//	    this.data = {length: 0};
-				if (("data" in self) === false) {
-					self.data_addbookmark = [];
-				} else if (self.data === undefined) {
-					self.data_addbookmark = [];
+				if (("data" in this) === false) {
+					this.data_addbookmark = [];
+				} else if (this.data === undefined) {
+					this.data_addbookmark = [];
 				}
-				self.data_addbookmark[0] = null; // null indicates a 'requested but not available yet'
+				this.data_addbookmark[0] = null; // null indicates a 'requested but not available yet'
 
-				self.req = $.ajax({
+				this.req = $.ajax({
 					data: send_data,
 					//		type: "POST",
 					type: "GET",
-					url: self.add_item_url,
+					url: this.add_item_url,
 					cache: true,
-					success: function (data) {
+					success:  (data) => {
 						// this;
-						self.onAddBookmarkSuccess(data);
+						this.onAddBookmarkSuccess(data);
 					},
-					error: function (XMLHttpRequest, textStatus, errorThrown) {
-						self.onAddBookmarkError(data);
+					error:  (XMLHttpRequest, textStatus, errorThrown) => {
+						this.onAddBookmarkError(data);
 					}
 				});
 			}, 50);
@@ -139,11 +137,11 @@
 			console.log("json.length=" + json.length);
 			if (json.length > 0) {
 				//	if (json.count > 0) {
-				var results = json
+				cost results = json
 				this.data_addbookmark.length = results.length;
-				for (var i = 0; i < results.length; i++) {
+				for (let i = 0; i < results.length; i++) {
 					console.log("i=" + i);
-					var item = results[i];
+					const item = results[i];
 
 					this.data[i] = {
 						index: i
@@ -161,28 +159,30 @@
 		}
 
 		onAddBookmarkError(json) {
-			alert("Xml:error onAddBookmarkError");
+			alert("Xml:onAddBookmarkError: onAddBookmarkError");
 		}
 
 		onSuccess(json, recStart) {
 			console.log("onSuccess");
 			console.log("json.length=" + json.length);
 			console.log("recStart=" + recStart);
-			var recEnd = recStart;
+			const recEnd = recStart;
 			if (json.length > 0) {
 				//	if (json.count > 0) {
-				var results = json
+				const results = json
 				recEnd = recStart + results.length;
 				this.data.length = Math.min(recEnd, 1000);
 				for (var i = 0; i < results.length; i++) {
 					console.log("i=" + i);
-					var item = results[i];
+					const item = results[i];
 
 					this.data[recStart + i] = {
 						index: recStart + i
 					};
 					this.data[recStart + i].id = item.id
 					this.data[recStart + i].name = item.name;
+					this.data[recStart + i].title = item.title;
+					this.data[recStart + i].authors = item.authors;
 					this.data[recStart + i].url = item.url;
 				}
 			}
@@ -195,7 +195,7 @@
 		}
 
 		onError(fromPage, toPage) {
-			alert("remotemodel-xml-b:error loading pages " + fromPage + " to " + toPage);
+			alert("remotemodel-xml-b:onError: loading pages " + fromPage + " to " + toPage);
 		}
 		/*
 			  RemoteModel.prototype.initialize = function initialize( id , name , desc , url , add_date ) {
@@ -205,12 +205,14 @@
 			  return super_inst;
 			  }
 		*/
+/*
 		initialize(id, name, desc, url, add_date) {
 			var super_inst = this.super_.initialize(id, name, desc, url, add_date);
 			super_inst.data_count = [];
 
 			return super_inst;
 		}
+*/
 	}
 
 	// Slick.Data.RemoteModel
