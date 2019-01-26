e481d66b6431d63af8521d283e0ec9dc69042ccd
yasuo kominami <ykominami@gmail.com>
2019-01-26 19:43:11 +0900

    make url with host and path

--- public/xml/slickx.js
+++ public/xml/slickx.js
@@ -3,7 +3,7 @@ import {
 } from './slick.remotemodel-xml.js';
 
 export const Slickx = class {
-    constructor(search_field, sgrid, score, upbtn, downbtn, acinput, items_count_url, items_url, add_item_url, delete_item_url) {
+    constructor(search_field, sgrid, score, upbtn, downbtn, acinput, host, item_count_url_path, item_url_path, add_item_url_path, delete_item_url_path) {
         this.selected_cell_row = null;
         this.selectedRow = null;
         this.search_field = search_field;
@@ -13,6 +13,10 @@ export const Slickx = class {
         this.downbtn = downbtn;
         this.acinput = acinput;
 
+        const item_count_url = `${host}${item_count_url_path}`
+        const item_url = `${host}${item_url_path}`
+        const add_item_url = `${host}${add_item_url_path}`
+        const delete_item_url = `${host}${delete_item_url_path}`
         //    var s;
         this.text_ // FIXME: eld = search_field;
         this.score = score;
@@ -70,13 +74,97 @@ export const Slickx = class {
 
         this.dataView = new Slick.Data.DataView();
 
-        this.loader = new RemoteModel(items_count_url, items_url, add_item_url, delete_item_url);
+        this.loader = new RemoteModel(item_count_url, item_url, add_item_url, delete_item_url);
 
         this.grid = new Slick.Grid(this.sgrid, this.dataView, this.columns, this.options);
 
         this.grid.setSelectionModel(new Slick.CellSelectionModel());
 
         this.dataView.setItems(this.loader.data);
+
+        this.set_dataview_event_handler();
+
+        this.set_grid_event_handler();
+
+        $("#contextMenu").click((e) => {
+            if (!$(e.target).is("li")) {
+                return;
+            }
+            if (!this.grid.getEditorLock().commitCurrentEdit()) {
+                return;
+            }
+            console.log(e.target.id);
+            if (e.target.id == "Delete") {
+                console.log(this.selectedRow);
+                this.data = this.dataView.getItem(this.selected_cell_row);
+                this.dataView.deleteItem(this.selectedRow);
+                this.loader.deleteData(this.data.id);
+            } else if (e.target.id == "Open") {
+                console.log(this.selectedRow);
+                this.data = this.dataView.getItem(this.selected_cell_row);
+                const childWindow = window.open('about:blank');
+                childWindow.location.href = this.data.url;
+                childWindow = null;
+            }
+        });
+        this.set_loader_event_handler( sgrid );
+
+        $(this.text_field).keyup((e) => {
+            // pushed Enter key
+            if (e.which == 13) {
+                //				this.do_search($(this).val());
+                const kv = $(this.text_field).val()
+                console.log("Call do_search|this.text_field=" + kv)
+                this.do_search(kv);
+            }
+        });
+        let kv = $(this.text_field).val()
+        if (typeof kv === 'undefined') {
+            kv = "";
+        }
+        console.log("kv=" + kv)
+        this.loader.setSearch(kv);
+        this.loader.setSort(this.score, -1);
+        this.grid.setSortColumn(this.score, false);
+        // load the first page
+        //    this.grid.onViewportChanged.notify();
+
+        this.set_btn_click_handler();
+        // AutoComplete
+        $(this.acinput).autocomplete({
+                source: (request, res) => {
+                    $.ajax({
+                        url: "/xml/api",
+                        type: "POST",
+                        cache: false,
+                        dataType: "json",
+                        data: {
+                            q: request.term
+                        },
+                        success: (o) => {
+                            res(o);
+                        },
+                        error: (xhr, ts, err) => {
+                            res(['']);
+                        }
+                    });
+                },
+                search: (event, ui) => {
+                    if (event.keyCode == 229) return false;
+                    return true;
+                },
+                open: () => {
+                    $(this).removeClass("ui-corner-all");
+                }
+            })
+            .keyup((event) => {
+                if (event.keyCode == 13) {
+                    $(this).autocomplete('#jquery-ui-autocomplete-input');
+                }
+            });
+    }
+
+    set_dataview_event_handler(){
         this.dataView.onRowCountChanged.subscribe((e, args) => {
             console.log("onRowCountChanged");
             this.grid.updateRowCount();
@@ -87,8 +175,9 @@ export const Slickx = class {
             this.grid.invalidateRows(args.rows);
             this.grid.render();
         });
+    }
 
-
+    set_grid_event_handler(){
         this.grid.onAddNewRow.subscribe((e, args) => {
             console.log("onAddNewRow");
             this.updateRow(args.item);
@@ -117,29 +206,26 @@ export const Slickx = class {
                 $("#contextMenu").hide();
             });
         });
-
-        $("#contextMenu").click((e) => {
-            if (!$(e.target).is("li")) {
-                return;
+    }
+    set_btn_click_handler(){
+        $(this.upbtn).click(() => {
+            this.page_num = this.page_num + 1;
+            const category_id = $("#txtSearch").val();
+            console.log("this.upbtn call do_search category_id=" + category_id)
+            do_search(category_id);
+        });
+        $(this.downbtn).click(() => {
+            if (this.page_num > 0) {
+                this.page_num = this.page_num - 1;
             }
-            if (!this.grid.getEditorLock().commitCurrentEdit()) {
-                return;
-            }
-            console.log(e.target.id);
-            if (e.target.id == "Delete") {
-                console.log(this.selectedRow);
-                this.data = this.dataView.getItem(this.selected_cell_row);
-                this.dataView.deleteItem(this.selectedRow);
-                this.loader.deleteData(this.data.id);
-            } else if (e.target.id == "Open") {
-                console.log(this.selectedRow);
-                this.data = this.dataView.getItem(this.selected_cell_row);
-                const childWindow = window.open('about:blank');
-                childWindow.location.href = this.data.url;
-                childWindow = null;
-            }
+            const category_id = $("#txtSearch").val();
+            console.log("this.downbtn call do_search category_id=" + category_id)
+            do_search(category_id);
         });
 
+    }
+
+    set_loader_event_handler( sgrid ){
         this.loader.onDataLoading.subscribe(() => {
             console.log("onDataLoading");
             if (!this.loadingIndicator) {
@@ -202,75 +288,7 @@ export const Slickx = class {
             console.log("onDeleteBookmarkDataLoaded=" + args.id);
         });
 
-        $(this.text_field).keyup((e) => {
-            // pushed Enter key
-            if (e.which == 13) {
-                //				this.do_search($(this).val());
-                const kv = $(this.text_field).val()
-                console.log("Call do_search|this.text_field=" + kv)
-                this.do_search(kv);
-            }
-        });
-        let kv = $(this.text_field).val()
-        if (typeof kv === 'undefined') {
-            kv = "";
-        }
-        console.log("kv=" + kv)
-        this.loader.setSearch(kv);
-        this.loader.setSort(this.score, -1);
-        this.grid.setSortColumn(this.score, false);
-        // load the first page
-        //    this.grid.onViewportChanged.notify();
-
-        $(this.upbtn).click(() => {
-            this.page_num = this.page_num + 1;
-            const category_id = $("#txtSearch").val();
-            console.log("this.upbtn call do_search category_id=" + category_id)
-            do_search(category_id);
-        });
-        $(this.downbtn).click(() => {
-            if (this.page_num > 0) {
-                this.page_num = this.page_num - 1;
-            }
-            const category_id = $("#txtSearch").val();
-            console.log("this.downbtn call do_search category_id=" + category_id)
-            do_search(category_id);
-        });
-
-        // AutoComplete
-        $(this.acinput).autocomplete({
-                source: (request, res) => {
-                    $.ajax({
-                        url: "/xml/api",
-                        type: "POST",
-                        cache: false,
-                        dataType: "json",
-                        data: {
-                            q: request.term
-                        },
-                        success: (o) => {
-                            res(o);
-                        },
-                        error: (xhr, ts, err) => {
-                            res(['']);
-                        }
-                    });
-                },
-                search: (event, ui) => {
-                    if (event.keyCode == 229) return false;
-                    return true;
-                },
-                open: () => {
-                    $(this).removeClass("ui-corner-all");
-                }
-            })
-            .keyup((event) => {
-                if (event.keyCode == 13) {
-                    $(this).autocomplete('#jquery-ui-autocomplete-input');
-                }
-            });
     }
-
 
     updateRow(item) {
         const name = item["name"];
